fd1f625b8910528e552c3678736b060451fb7fe9
Mhijazi16 <gultimate0@gmail.com>
2024-02-26 23:26:04 +0200

    Constant.h : adding text related variables (colors/styles)

---
+++ Constants.h
@@ -0,0 +1,11 @@
+
+#define PURPLE "\033[35m"
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define GRAY    "\033[90m"
+#define BOLD    "\033[1m"
