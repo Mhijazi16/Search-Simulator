540552b5b289821a150415aaf7b09b581d11bf49
Mhijazi16 <gultimate0@gmail.com>
2024-02-27 22:10:19 +0200

    GameContext : create RefreshContext() method to reassign Possitions

--- GameContext.h
+++ GameContext.h
@@ -32,6 +32,25 @@ class GameContext{
       Backup = Map;
       row_limit = Map.size();
       col_limit = Map[0].size();
+      RefreshContext();
     }
+
+  void RefreshContext(){
+
+    if(!Meat.empty() || !Boxes.empty()){
+      Meat.clear(); 
+      Boxes.clear();
+    }
+
+    for (int i = 0; i < row_limit; i++) 
+      for (int j = 0; j < col_limit; j++) {
+        if(Map[i][j] == MEAT)
+          Meat.push_back(Point(i,j));
+        else if(Map[i][j] == PLAYER)
+          Player = Point(i,j);
+        else if(Map[i][j] == BASKET)
+          Boxes.push_back(Point(i,j));
+      }
+  }
 };
 
