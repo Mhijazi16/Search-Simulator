ba0303c9e4ab91f4888e188876acf399cb530ef3
Mhijazi16 <gultimate0@gmail.com>
2024-02-29 15:55:13 +0200

    HumanStrategy : Implementing HumanStrategy

--- HumanStrategy.h
+++ HumanStrategy.h
@@ -1,6 +1,36 @@
 #include "ISimulateStrategy.h"
 #include "Movement.h"
 
-class HumanStrategy : ISimulateStrategy, Movement{
+class HumanStrategy : public ISimulateStrategy, Movement{
+
+  private:
+    char input; 
+  public:
+    void Simulate() override{
+
+      GamePrinter::PrintMap(); 
+      while (Meat.size() != score && !Lost) {
+
+        GamePrinter::PrintMap();
+        cin >> input; 
+
+        if(input == 'w')
+          MoveTo(Player,Player.Top());
+        else if(input == 's')
+          MoveTo(Player,Player.Bottom());
+        else if(input == 'a')
+          MoveTo(Player,Player.Left());
+        else if(input == 'd')
+          MoveTo(Player,Player.Right());
+        else 
+          break;
+      }
+
+      if(!Lost)
+        GamePrinter::GameWon();
+
+    }
 
 };
+
+
