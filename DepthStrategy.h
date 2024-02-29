4269d21cb19cdefd3c88542a48e5dc2d8749f588
Mhijazi16 <gultimate0@gmail.com>
2024-02-29 16:08:09 +0200

    DepthAgent : create RefreshDirections() mtehod to populate the Directions srounding the Player

--- DepthStrategy.h
+++ DepthStrategy.h
@@ -4,7 +4,21 @@
 
 class DepthStrategy: public ISimulateStrategy, Movement{
 
+  private:
+    vector<Point> Directions; 
+  public: 
+
+  void RefreshDirections(){
+    if(!Directions.empty())
+      Directions.clear();
+
+    Directions.push_back(Player.Top());
+    Directions.push_back(Player.Left());
+    Directions.push_back(Player.Bottom());
+    Directions.push_back(Player.Right());
+  }
+  
   void Simulate() override{
-    
+    RefreshDirections();
   }
 };
