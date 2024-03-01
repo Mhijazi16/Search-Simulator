45f174b9de543a6f2083de4d59fe226778c57e1b
Mhijazi16 <gultimate0@gmail.com>
2024-03-01 23:12:45 +0200

    DepthAgent : Finishing the Implementation of DepthAgen

--- DepthStrategy.h
+++ DepthStrategy.h
@@ -14,34 +14,79 @@ class DepthStrategy: public ISimulateStrategy, Movement{
   private:
     vector<Point> Directions = {Point(-1,0),Point(0,1),Point(1,0),Point(0,-1)}; 
     vector<Point> Path;
+    vector<Point> BoxPath;
     int Distance = 0;
+    int index = 0;
     bool done = false;
 
   public: 
 
-    void ExecutionSpeed()
-    {
+    void ExecutionSpeed(){
       while (!done) cin >> Delay; 
     }
 
     void Execution(){
-      SimulatePlayerPath();
+
+      SimulteBoxPath();
+      BoxPath = vector<Point>(Path.begin(),Path.end());
+      BoxPath.push_back(Meat[index]);
+      Path.clear();
+
+      for (int i = 1; i < BoxPath.size(); i++) {
+
+        SimulatePlayerPath(BoxPath[i]);
+        for (auto point : Path) {
+          MoveTo(Player, point);
+          Delay = 300;
+          GamePrinter::PrintMap();
+        }
+        Path.clear();
+        if(!CanBeMoved(Player, Boxes[index], true)){
+          GamePrinter::GameLost();
+          return; 
+        }
+        Boxes[index] = BoxPath[i];
+      }
+
+      done = true;
     }
 
-    void SimulatePlayerPath(){
+    void SimulteBoxPath(){
 
-      auto Target = Boxes[0].GetPushPoint(Meat[0]);
-      Distance = Point::GetDistance(Player, Target);
-      auto next = GameContext::EmptyLocation(Player);
 
-      while (!DFS(Player,next,Target)){
+      auto Target = CloserDirection(Meat[index], Boxes[index]);
+      Point next = CloserDirection(Boxes[index], Target);
+      Distance = Point::GetDistance(Boxes[index], Target);
+
+      if(Distance == 0){
+        Path.push_back(Boxes[index]);
+        return;
+      }
+
+      while (!DFS(Boxes[index],next,Target)){
         Distance++;
         Delay = 200;
         GamePrinter::LimitExceeded(Distance);
         sleep_for(seconds(2));
       }
 
-      done = true;
+      GameContext::MapAt(Target) = TEST;
+      Path.push_back(Target);
+    }
+
+    void SimulatePlayerPath(Point& Dest){
+
+      auto Target = Boxes[index].GetPushPoint(Dest);
+      Distance = Point::GetDistance(Player, Target);
+      auto next = CloserDirection(Player, Target); 
+
+      while (!DFS(Player,next,Target) && *(Path.end()-1) != Target){
+        Distance++;
+        Delay = 200;
+        GamePrinter::LimitExceeded(Distance);
+        sleep_for(seconds(2));
+      }
+
       GameContext::MapAt(Target) = TEST;
       Path.push_back(Target);
     }
@@ -49,22 +94,27 @@ class DepthStrategy: public ISimulateStrategy, Movement{
     bool DFS(Point& Source,Point& Direction,Point& Target){
 
       GamePrinter::PrintMap();
+      cout << PLAYER << ": " << Player << " " << BASKET << ": " << Boxes[index] << endl;
+      cout << RED << "Current : " <<  Source << endl; 
+      cout << GREEN << "Next : " <<  Direction << endl; 
+      cout << CYAN << "Target :" << Target << RESET << endl;
 
       if(Source == Target)
         return true;
 
       string& cell = GameContext::MapAt(Direction);
 
-      if(cell != "  " || Path.size() == Distance )
+      if(cell == TEST)
+        return false;
+
+      if(cell != "  " || Path.size() > Distance )
         return false;
 
       if(!CanBeMoved(Source,Direction,false))
         return false;
 
-      if(cell != PLAYER && cell != BASKET){
-        cell = TEST;
-        Path.push_back(Source);
-      }
+      cell = TEST;
+      Path.push_back(Source);
 
       for (auto Side : Directions) 
       {
@@ -73,32 +123,25 @@ class DepthStrategy: public ISimulateStrategy, Movement{
           return true;
       }
 
-      if(cell != PLAYER){
-        cell = "  "; 
-        Path.pop_back();
-      }
+      cell = "  "; 
+      Path.pop_back();
 
       return false;
     }
 
-    void Simulate() override
-    {
-      thread Speed([this]{this->ExecutionSpeed();});
-      thread Execute([this]{this->Execution();});
+    void Simulate() override {
 
-      Speed.join();
-      Execute.join();
+      int len = Boxes.size();
+      for (; index < len; index++) {
+        done = false;
+        thread Speed([this]{this->ExecutionSpeed();});
+        thread Execute([this]{this->Execution();});
 
-      for (auto point : Path) {
-        MoveTo(Player, point);
-        Delay = 300;
-        GamePrinter::PrintMap();
+        Speed.join();
+        Execute.join();
       }
 
-      if(!CanBeMoved(Player, Boxes[0], true)){
-        GamePrinter::GameLost();
-        return; 
-      }
+
 
       GamePrinter::CongratsMap();
       sleep_for(seconds(1));
