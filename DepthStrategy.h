40ea334304694c472e289fe9f232402fec5d533d
Mhijazi16 <gultimate0@gmail.com>
2024-03-01 18:14:38 +0200

    DepthAgent : override Simulate() method for DepthStrategy

--- DepthStrategy.h
+++ DepthStrategy.h
@@ -1,24 +1,56 @@
 #include "ISimulateStrategy.h"
 #include "Movement.h"
+#include <chrono>
+#include <functional>
+#include <set>
+#include <unordered_map>
 #include <vector>
+#include <thread>
+
+using namespace std; 
 
 class DepthStrategy: public ISimulateStrategy, Movement{
 
   private:
-    vector<Point> Directions; 
+    vector<Point> Directions = {Point(-1,0),Point(0,1),Point(1,0),Point(0,-1)}; 
+    vector<Point> Path;
+    int Distance = 0;
+    bool done = false;
+
   public: 
 
-  void RefreshDirections(){
-    if(!Directions.empty())
-      Directions.clear();
+    void ExecutionSpeed()
+    {
+      // while (!done) cin >> Delay; 
+    }
 
-    Directions.push_back(Player.Top());
-    Directions.push_back(Player.Left());
-    Directions.push_back(Player.Bottom());
-    Directions.push_back(Player.Right());
-  }
+    void Execution(){
+      // SimulatePlayerPath();
+    }
+
+    void Simulate() override
+    {
+      thread Speed([this]{this->ExecutionSpeed();});
+      thread Execute([this]{this->Execution();});
+
+      Speed.join();
+      Execute.join();
+
+      for (auto point : Path) {
+        MoveTo(Player, point);
+        Delay = 300;
+        GamePrinter::PrintMap();
+      }
+
+      if(!CanBeMoved(Player, Boxes[0], true)){
+        GamePrinter::GameLost();
+        return; 
+      }
+
+      GamePrinter::CongratsMap();
+      sleep_for(seconds(1));
+      GamePrinter::GameWon();
+
+    }
   
-  void Simulate() override{
-    RefreshDirections();
-  }
 };
